2e003a7af0946c8100f457747b81cd5470086bb9
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2024-02-13 15:37:34 -0300

    patch: added blazor tags and validation

--- pwa/CarnaCode.Core/Domain/Contracts/ILogin.cs
+++ pwa/CarnaCode.Core/Domain/Contracts/ILogin.cs
@@ -1,15 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarmaCode.Core.Domain.Contracts;
 
 public record LoginInput
 {
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
-    public bool ImNotARobot { get; set; }
+  [Required(ErrorMessage = "Informe seu email")]
+  public string Email { get; set; } = default!;
+
+  [Required(ErrorMessage = "Informe sua senha")]
+  public string Password { get; set; } = default!;
+
+  public bool ImNotARobot { get; set; }
 }
 
 public record LoginOutput
 {
-    public Auth Auth { get; set; } = default!;
+  public Auth Auth { get; set; } = default!;
 }
 
 public interface ILogin : IUseCase<LoginInput, LoginOutput>
