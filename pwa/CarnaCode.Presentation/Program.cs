746fe30c028a19261fed4357467f63a58cbfade0
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2024-02-13 23:21:00 -0300

    fix: navigator and actions

--- pwa/CarnaCode.Presentation/Program.cs
+++ pwa/CarnaCode.Presentation/Program.cs
@@ -12,7 +12,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton(sp => new NavigationManager(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped<ICreateUser, CreateUserUseCase>();
 builder.Services.AddScoped<IUserRepository, UserLocalStorageRepository>();
